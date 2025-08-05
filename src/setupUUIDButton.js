710587567de9bb05547adf5ee2d16c29a0b4a2a7
Silje32 <msh070425@gmail.com>
2025-08-05 09:55:36 +0200

    Changed the name on my file and my function

--- src/setupUUIDButton.js
+++ src/setupUUIDButton.js
@@ -1,6 +1,6 @@
 import { v4 as uuidv4 } from "uuid";
 
-const createUUIDButton = (buttonElement) => {
+export const setupUUIDButton = (buttonElement) => {
   buttonElement.addEventListener("click", () => {
     const uuid = uuidv4();
     alert("Your UUID is: ${uuid}");
