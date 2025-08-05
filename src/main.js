57ab14b576530f0a51efdd30af730168d75e52d1
Silje32 <msh070425@gmail.com>
2025-08-05 10:11:41 +0200

    imported the setupUUIDButton and added the button

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
+import javascriptLogo from "./javascript.svg";
+import viteLogo from "/vite.svg";
+import { setupCounter } from "./counter.js";
+import { setupUUIDButton } from "./setupUUIDButton.js";
 
-document.querySelector('#app').innerHTML = `
+document.querySelector("#app").innerHTML = `
   <div>
     <a href="https://vite.dev" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
@@ -14,11 +15,13 @@ document.querySelector('#app').innerHTML = `
     <h1>Hello Vite!</h1>
     <div class="card">
       <button id="counter" type="button"></button>
+       <button id="uuidButton" type="button">Generate UUID</button>
     </div>
     <p class="read-the-docs">
       Click on the Vite logo to learn more
     </p>
   </div>
-`
+`;
 
-setupCounter(document.querySelector('#counter'))
+setupCounter(document.querySelector("#counter"));
+setupUUIDButton(document.querySelector("#uuidButton"));
